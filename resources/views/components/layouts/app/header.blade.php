815cf4b6ab7fc6597143049af84c663593f4db70
Yeasir Arafat <yeasirarafat321@gmail.com>
2026-03-03 13:01:45 +0600

    style: Add responsive horizontal padding to the header and remove the `container` attribute.

--- resources/views/components/layouts/app/header.blade.php
+++ resources/views/components/layouts/app/header.blade.php
@@ -4,7 +4,7 @@
         @include('partials.head')
     </head>
     <body class="min-h-screen bg-white dark:bg-zinc-800 flex flex-col">
-        <flux:header container class="border-b border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
+        <flux:header class="px-4 sm:px-6 lg:px-8 border-b border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
             <flux:navbar class="-mb-px max-lg:hidden">
                 <flux:navbar.item icon="home" :href="route('home')" :current="request()->routeIs('home')" wire:navigate>
                     {{ __('Home') }}
